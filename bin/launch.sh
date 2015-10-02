b960a53a6fdf1322f4efc3e1cf5eb8e711afcd83
Ron Kurr <kurr@kurron.org>
2015-10-02 14:04:40 +0000

    Emit JSON log messages

--- bin/launch.sh
+++ bin/launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This script will launch the application using the default profile
-$JAVA_HOME/bin/java -jar build/libs/monitor-postgresql-1.0.0.RELEASE.jar
+$JAVA_HOME/bin/java -jar build/libs/monitor-postgresql-2.2.0.RELEASE.jar
